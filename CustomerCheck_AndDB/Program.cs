7dab75a59d5d3c10c154d2048f8dc96190fdf239
Bilal Can Altındağ <bilalcanaltindag@hotmail.com>
2022-03-23 20:27:52 +0300

    Service and manager added

---
+++ CustomerCheck_AndDB/Program.cs
@@ -0,0 +1,16 @@
+﻿using CustomerCheck_AndDB.Abstract;
+using CustomerCheck_AndDB.Concrete;
+using CustomerCheck_AndDB.Entities;
+using System;
+
+namespace CustomerCheck_AndDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager();
+            customerManager.Save(new Customer { NationalityId = "28861499108", FirstName = "Engin", LastName = "Demiroğ" , DateOfBirth = new DateTime(1985,1,6) });
+        }
+    }
+}
